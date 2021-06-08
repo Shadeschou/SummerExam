c099cdfa46ff54ab7e50472f32a03f17aa8ff7dd
AljoschaEASV <aljo0025@easv365.dk>
2021-06-08 13:01:26 +0200

    feature added!

--- TheOxbridgeWebsite/src/app/components/forgot-password/forgot-password.component.ts
+++ TheOxbridgeWebsite/src/app/components/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ForgotPasswordService} from '../../services/forgot-password.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent implements OnInit {
+  public mail: string;
 
-  constructor() { }
+  constructor(private forgotPasswordService: ForgotPasswordService) { }
 
   ngOnInit(): void {
   }
 
+  OnSubmit() {
+    console.log(this.mail);
+    this.forgotPasswordService.resetPass(this.mail);
+  }
 }
