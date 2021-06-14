92c7ee8296259efb2cd1ae8d6391ccaed737cbf6
kasper schou <capper2650@hotmail.com>
2021-06-14 10:15:55 +0200

    Flutter Password Reset

---
+++ Flutter/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\cappe\\Desktop\\oxbridge" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\cappe\\Desktop\\oxbridge"
+  "FLUTTER_ROOT=C:\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\cappe\\Desktop\\oxbridge\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\cappe\\Desktop\\oxbridge"
+  "FLUTTER_TARGET=C:\\Users\\cappe\\Desktop\\oxbridge\\lib\\main.dart"
+  "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\cappe\\Desktop\\oxbridge\\.dart_tool\\package_config.json"
+)
