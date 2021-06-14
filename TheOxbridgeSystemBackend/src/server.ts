8ac2821f87213568505908a9ac79fdd4ab545eb9
AljoschaEASV <55789647+AljoschaEASV@users.noreply.github.com>
2021-06-14 14:42:03 +0200

    Feature/profile validation (#18)
    * Added the Forgot Password
    
    * Done the requirement
    
    * Minor bugfix
    
    * Added minor fixes-The Function-More loose Coupled classes
    
    * saving the web
    
    * pleaseWork
    
    * Changed the html erroor aaggain
    
    * Added the route for Broadcast get
    
    * Added the route for Broadcast get - And now used add . too.

--- TheOxbridgeSystemBackend/src/server.ts
+++ TheOxbridgeSystemBackend/src/server.ts
@@ -19,10 +19,11 @@ import nodemailer from "nodemailer";
 import date from "date-and-time";
 import * as crypto from "crypto";
 import {timerForTheReminder} from "./controllers/checkEvents";
-timerForTheReminder();
+import {Broadcast, IBroadcast} from "./models/broadcast";
+import {BroadcastService} from "../../TheOxbridgeWebsite/src/app/services/broadcast.service";
 import { Broadcast } from "./models/broadcast";
 
-
+timerForTheReminder();
 
 dotenv.config({path: 'configs/config.env'});
 const app = express();
@@ -30,11 +31,9 @@ app.use(cookieParser(process.env.TOKEN_SECRET));
 app.use(cors());
 const router = express.Router();
 
-
 const urlencode = bodyParser.urlencoded({extended: true});
 app.use(express.static('public'));
 app.use(bodyParser.json());
-
 
 connect(process.env.DB, {
     useNewUrlParser: true,
@@ -672,7 +671,7 @@ app.post('/users/forgot/:emailUsername', async (req, res) => {
         });
         console.info()
         console.log("After Send");
-        res.status(202).json(user);
+        res.status(202).json({message: "new pw sent"});
     } catch (e) {
         res.status(400).json('Sent random pw failed.')
     }
@@ -1004,6 +1003,22 @@ app.post('/broadcast', async (req, res) => {
         res.status(400).json('BAD REQUEST');
     }
 });
+
+app.post('/getterForBroadcast', async (req, res) => {
+    try {
+        //getting the broadcasts from the db. Using the email to connect to the user.
+        const username: any = req.body.Username;
+        const broadcast: IBroadcast[] = await Broadcast.find({emailUsername: username}, {_id: 0,__v:0});
+        await Broadcast.deleteMany({emailUsername: username});
+        res.status(200).json({message: 'Found the Broadcast - Deleting by email.'});
+
+    }
+catch(e){
+        res.status(400).json("BAD REQUEST")
+}
+    });
+
+
 
 app.put('/eventRegistrations/updateParticipant/:eventRegId', async (req, res) => {
     // Checking if authorized
--- TheOxbridgeSystemBackend/src/tests/requirements.test.ts
+++ TheOxbridgeSystemBackend/src/tests/requirements.test.ts
@@ -1,7 +1,7 @@
 import {app} from "../server";
 import request from "supertest";
-
 import {timerForTheReminder} from "../controllers/checkEvents";
+import {IBroadcast} from "../models/broadcast";
 
 const api = app
 describe("test", () => {
@@ -37,9 +37,31 @@ describe("Finally able to test the post....",  () => {
 })
 
 describe("Forgot Password", () => {
+
+  it("Should run with the Mail given ", async () => {
+        const result = await request(api).post('/users/forgot/aljo0025@easv365.dk');
+        expect(result.body).toEqual({message: 'new pw sent'});
+        expect(result.status).toEqual(202);
+    });
+});
+
+describe("Broadcast feature", () => {
+    it("Should post broadcast to DB ", async () => {
+        const result = await request(api).post('/broadcast').send({eventId: 1, message: "asdf"});
+        expect(result.body).toEqual({message:'Broadcast successfully sent'});
+        expect(result.status).toEqual(201);
+    });
+    it("Should GET the made Broadcast", async () => {
+        const result = await request(api).post('/getterForBroadcast').send({emailUsername: "emailUsername"});
+        expect(result.body).toEqual({message: 'Found the Broadcast - Deleting by email.'});
+        expect(result.status).toEqual(200);
+    });
+});
+
     it("should run with ID given", async () => {
         const result = await request(api).delete("/eventregistrations/1");
         expect(result.body).toEqual({message: 'Registration Deleted'});
         expect(result.status).toEqual(202);
     });
 });
+
