f3b13f34442acd61a2527523347fbc596b03609c
Kasper Jørgensen <44469647+Shadeschou@users.noreply.github.com>
2021-06-14 10:10:32 +0200

    working flutter (#8)

---
+++ Flutter/android/app/src/main/kotlin/com/example/oxbridge/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.oxbridge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter/lib/model/event.dart
@@ -0,0 +1,38 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+
+class Event {
+  final num eventId;
+  final String name;
+  final String eventStart;
+  final String eventEnd;
+  final String city;
+  final String eventCode;
+  final String actualEventStart;
+  final bool isLive;
+
+  Event(
+      {@required this.eventId,
+      @required this.name,
+      @required this.eventStart,
+      @required this.eventEnd,
+      @required this.city,
+      @required this.eventCode,
+      @required this.actualEventStart,
+      @required this.isLive
+  });
+
+  factory Event.fromJson(Map<String, dynamic> json) {
+    return Event(
+      eventId: json['eventId'] as num,
+      name: json['name'] as String,
+      eventStart: json['eventStart'] as String,
+      eventEnd: json['eventEnd'] as String,
+      city: json['city'] as String,
+      eventCode: json['eventCode'] as String,
+      actualEventStart: json['actualEventStart'] as String,
+      isLive: json['isLive'] as bool
+    );
+  }
+}
