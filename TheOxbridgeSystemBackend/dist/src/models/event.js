454ad884fe6db3f07bb57dc155c7886ece35d2b0
Kasper Jørgensen <44469647+Shadeschou@users.noreply.github.com>
2021-06-14 10:13:29 +0200

    PostBroadcast (#9)

---
+++ TheOxbridgeSystemBackend/dist/src/models/event.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EventModel = void 0;
+const mongoose_1 = require("mongoose");
+const IEventSchema = new mongoose_1.Schema({
+    eventId: { type: Number },
+    name: { type: String },
+    eventStart: { type: Date },
+    eventEnd: { type: Date },
+    city: { type: String },
+    eventCode: { type: String },
+    actualEventStart: { type: Date },
+    isLive: { type: Boolean }
+});
+const EventModel = mongoose_1.model('Event', IEventSchema);
+exports.EventModel = EventModel;
+//# sourceMappingURL=event.js.map
